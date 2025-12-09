e19bdda04e1fb9950bf979f3a5294bc9db206a44
USER <user@USERs-MacBook-Pro.local>
2025-12-09 21:38:54 +0900

    first commit

---
+++ FreeToGame/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FreeToGame
+//
+//  Created by USER on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreeToGame/FreeToGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeToGameApp.swift
+//  FreeToGame
+//
+//  Created by USER on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeToGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
